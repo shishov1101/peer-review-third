9af75b5f56df5814aa54dbd43e0f198062c063f6
shishov1101 <shishov1101@gmail.com>
2023-12-13 23:57:04 +0300

    First code review

--- LoadPerson.cpp
+++ LoadPerson.cpp
@@ -1,25 +1,51 @@
-vector<Person> LoadPersons(string_view db_name, int db_connection_timeout, bool db_allow_exceptions,
-                           DBLogLevel db_log_level, int min_age, int max_age, string_view name_filter) {
-    DBConnector connector(db_allow_exceptions, db_log_level);
+struct ConnectionParamsOfDB {
+    bool db_allow_exceptions;
+    DBLogLevel db_log_level;
+    string_view db_name;
+    int db_connection_timeout;
+};
+
+struct QueryFilterParams {
+    int min_age;
+    int max_age;
+    string_view name_filter;
+};
+
+std::optional<DBHandler> GetDBHanler(const ConnectionParamsOfDB &connection_db_params) {
+    DBConnector connector(connection_db_params.db_allow_exceptions, connection_db_params.db_log_level);
     DBHandler db;
-    if (db_name.starts_with("tmp."s)) {
-        db = connector.ConnectTmp(db_name, db_connection_timeout);
+    if (connection_db_params.db_name.starts_with("tmp."s)) {
+        db = connector.ConnectTmp(connection_db_params.db_name, connection_db_params.db_connection_timeout);
     } else {
-        db = connector.Connect(db_name, db_connection_timeout);
+        db = connector.Connect(connection_db_params.db_name, connection_db_params.db_connection_timeout);
     }
-    if (!db_allow_exceptions && !db.IsOK()) {
-        return {};
+    if (!connection_db_params.db_allow_exceptions && !db.IsOK()) {
+        return nullopt;
     }
 
+    return db;
+}
+
+DBQuery GetPersonDBQuery(const QueryFilterParams &query_filter_params, const DBHandler& db_handler) {
     ostringstream query_str;
     query_str << "from Persons "s
               << "select Name, Age "s
-              << "where Age between "s << min_age << " and "s << max_age << " "s
-              << "and Name like '%"s << db.Quote(name_filter) << "%'"s;
+              << "where Age between "s << query_filter_params.min_age << " and "s << query_filter_params.max_age << " "s
+              << "and Name like '%"s << db_handler.Quote(query_filter_params.name_filter) << "%'"s;
     DBQuery query(query_str.str());
+}
+
+vector<Person> LoadPersons(const ConnectionParamsOfDB &connection_db_params, const QueryFilterParams &query_filter_params) {
+    auto db_handler = GetDBHanler(connection_db_params);
+    if (!db_handler)
+    {
+        return {};
+    }
+
+    auto query = GetPersonDBQuery(query_filter_params, db_handler);
 
     vector<Person> persons;
-    for (auto [name, age] : db.LoadRows<string, int>(query)) {
+    for (auto [name, age] : db_handler.LoadRows<string, int>(query)) {
         persons.push_back({move(name), age});
     }
     return persons;
